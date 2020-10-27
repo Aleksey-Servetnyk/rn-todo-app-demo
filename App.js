98eed37ef7a24837b78d64609371fbf82fd80222
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-10-27 06:28:00 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo'
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const AddTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    setTodos(prev => [...prev, {
+      id: Date.now().toString(),
+      title
+    }])
+  }
+
+  return (
+    <View>
+      <Navbar title="Todo App"/>
+      <View style={styles.container}>
+        {/* <AddTodo onSubmit={AddTodo} />
+
+          <View>
+            {todos.map(todo => {
+              return (<Text>{todo.title}</Text>)
+            })}
+          </View> */}
+
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
+});
