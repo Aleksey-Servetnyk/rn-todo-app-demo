61f26e90c3470c5217a79ac02de31debea726927
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-10-28 05:55:40 +0200

    list todo rendering

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import { Navbar } from './src/Navbar'
 import { AddTodo } from './src/AddTodo'
 import { Todo } from './src/Todo'
@@ -26,11 +26,17 @@ export default function App() {
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
 
-          <ScrollView>
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} /> }
+        />
+
+          {/* <ScrollView>
             {todos.map(todo => (
               <Todo todo={todo} key={todo.id} /> 
             ))}
-          </ScrollView>
+          </ScrollView> */}
 
       </View>
       <StatusBar style="auto" />
