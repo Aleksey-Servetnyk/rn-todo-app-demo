f07c689cc8ea986d563b72824704d8f4848224ab
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-10-31 16:56:28 +0200

    creating of reusable componrnts ended

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { AppText } from './ui/AppText'
 
 export const Todo = ({ todo, onRemove, onOpen }) => {
     // const longPressHandler = () => {
@@ -15,7 +16,7 @@ export const Todo = ({ todo, onRemove, onOpen }) => {
             onLongPress={onRemove.bind(null, todo.id)}
         >
             <View style={styles.todo}>
-                <Text style={styles.title}>{todo.title}</Text>
+                <AppText>{todo.title}</AppText>
             </View>
         </TouchableOpacity>
     )
@@ -30,8 +31,5 @@ const styles = StyleSheet.create ({
         borderColor: 'grey',
         borderRadius: 5,
         marginBottom:10
-    },
-    title: {
-        fontFamily: 'roboto-bold'
     }
 })
