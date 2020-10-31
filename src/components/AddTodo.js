2ba9d3492663236ef6dd4acd4a5c2aa4f5976084
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-10-31 08:57:15 +0200

    image introduction ended

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { View, StyleSheet, TextInput, Button, Alert} from 'react-native'
 import { THEME } from '../theme'
 
-export const AddTodo = ({ onSubmit}) => {
+export const AddTodo = ({ onSubmit }) => {
     const[value, setValue] = useState('') 
 
     const pressHandler = () => {
