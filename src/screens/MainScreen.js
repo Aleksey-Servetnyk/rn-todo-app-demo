d61b968cb47bc3f6af000a992ceb16efb9a0f6d2
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-01 08:45:27 +0200

    adaptation of screen orientation ended

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import { StyleSheet, View, FlatList, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, FlatList, Image, Dimensions } from 'react-native'
 import { AddTodo } from '../components/AddTodo'
 import { Todo } from '../components/Todo'
+import { THEME } from '../theme'
 
 export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+    const [deviceWidth, setDeviceWidth] = useState(
+            Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
+        )
 
+    useEffect(() => {
+        const update = () => {
+            const width = Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
+            setDeviceWidth(width)
+        }
+        Dimensions.addEventListener('change', update)
+        return () => {
+            Dimensions.removeEventListener('change', update)
+        }
+    })
+    //const width = Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
     let content
 
     if (todos.lenght == 0) {
@@ -17,11 +32,13 @@ export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
             </View>
         )} else {
             content =(
-                <FlatList
-                    keyExtractor={item => item.id.toString()}
-                    data={todos}
-                    renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} /> }
-                />
+                <View style={{ deviceWidth }}>
+                    <FlatList
+                        keyExtractor={item => item.id.toString()}
+                        data={todos}
+                        renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} /> }
+                    />
+                </View>
         )}
 
     return (
