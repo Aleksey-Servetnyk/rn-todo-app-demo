17055712882544097044ec4ffe8b9013be4c4e33
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-01 19:49:51 +0200

    creating of the reducer ended

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
-import React, {useState} from 'react';
-import * as Font from 'expo-font'
-import { AppLoading } from 'expo'
-import { MainLayout } from './src/MainLayout'
-import { TodoState } from './src/context/todo/TodoState'
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { MainLayout } from "./src/MainLayout";
+import { TodoState } from "./src/context/todo/TodoState";
 
-async function loadApplication () {
+async function loadApplication() {
   await Font.loadAsync({
-    'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
-    'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf')
-  })
+    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
 }
 
 export default function App() {
-  const [isRedy, setIsRedy] = useState(false)     // app redy flug state
-  if (!isRedy) {                                  // waiting load the application
+  const [isRedy, setIsRedy] = useState(false); // app redy flug state
+  if (!isRedy) {
+    // waiting load the application
     return (
-      <AppLoading 
-        startAsync={loadApplication} 
-        onError={err => console.log(err)}
+      <AppLoading
+        startAsync={loadApplication}
+        onError={(err) => console.log(err)}
         onFinish={() => setIsRedy(true)}
       />
-    )
+    );
   }
 
   return (
-    <TodoState><MainLayout /></TodoState>
+    <TodoState>
+      <MainLayout />
+    </TodoState>
   );
 }
-
--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,97 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useContext } from 'react'
-import { View, StyleSheet } from 'react-native'
-import { Navbar } from './components/Navbar'
-import { THEME } from './theme';
-import { MainScreen } from './screens/MainScreen'
-import { TodoContext } from './context/todo/todoContext'
-import { TodoScreen } from './screens/TodoScreen'
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { Navbar } from "./components/Navbar";
+import { THEME } from "./theme";
+import { MainScreen } from "./screens/MainScreen";
+import { TodoContext } from "./context/todo/todoContext";
+import { TodoScreen } from "./screens/TodoScreen";
 
 export const MainLayout = () => {
-    const todoContext = useContext(TodoContext)
-    const [todoId, setTodoId] = useState(null)      // screen navigation state
-    const [todos, setTodos] = useState([])          // todos content state
+  const { todos, addTodo, removeTodo, updateTodo } = useContext(TodoContext);
+  const [todoId, setTodoId] = useState(null); // screen navigation state
+  // const [todos, setTodos] = useState([]); // todos content state
 
-    const addTodo = (title) => {
-        // const newTodo = {
-        //   id: Date.now().toString(),
-        //   title: title
-        // }
-    
-        setTodos(prev => [...prev, {
-          id: Date.now().toString(),
-          title
-        }])
-      }
-    
-      const removeTodo = (id) => {
-        const todo = todos.find(t => t.id === id)
-        Alert.alert(
-          "Deleting the element",
-          `Are you sure you want to delete the "${todo.title}" ?`,
-          [
-            {
-              text: "Cancel",
-              style: "cancel"
-            },
-            { text: "Delete",
-              onPress: () => {
-                setTodoId(null)                                       // Back to main screen
-                setTodos(prev => prev.filter(todo => todo.id != id))
-              } 
-            }
-          ],
-          { cancelable: false }
-        );
-      }
-    
-      const updateTodo = (id, title) => {             // update title by id
-        setTodos(old => old.map(todo => {
-          if (todo.id === id) {
-            todo.title = title
-          }
-          return todo
-        }))
-      }
-    
-      let content = (
-        <MainScreen 
-          todos={todoContext.todos} 
-          addTodo={addTodo} 
-          removeTodo={removeTodo} 
-          // openTodo={(id) => {
-          //   setTodoId(id)
-          // }} 
-          openTodo={setTodoId}
-        />
-      )
-    
-      if (todoId) {
-        const selectedTodo = todos.find(todo => todo.id === todoId)
-        content = (
-          <TodoScreen 
-            onRemove={removeTodo} 
-            goBack={() => setTodoId(null)} 
-            todo={selectedTodo} 
-            onSave={updateTodo}  
-          />
-        )
-      }
+  // const addTodo = (title) => {
+  //   // const newTodo = {
+  //   //   id: Date.now().toString(),
+  //   //   title: title
+  //   // }
+
+  //   setTodos((prev) => [
+  //     ...prev,
+  //     {
+  //       id: Date.now().toString(),
+  //       title,
+  //     },
+  //   ]);
+  // };
+
+  // const removeTodo = (id) => {
+  //   const todo = todos.find((t) => t.id === id);
+  //   Alert.alert(
+  //     "Deleting the element",
+  //     `Are you sure you want to delete the "${todo.title}" ?`,
+  //     [
+  //       {
+  //         text: "Cancel",
+  //         style: "cancel",
+  //       },
+  //       {
+  //         text: "Delete",
+  //         onPress: () => {
+  //           setTodoId(null); // Back to main screen
+  //           setTodos((prev) => prev.filter((todo) => todo.id != id));
+  //         },
+  //       },
+  //     ],
+  //     { cancelable: false }
+  //   );
+  // };
+
+  // const updateTodo = (id, title) => {
+  //   // update title by id
+  //   setTodos((old) =>
+  //     old.map((todo) => {
+  //       if (todo.id === id) {
+  //         todo.title = title;
+  //       }
+  //       return todo;
+  //     })
+  //   );
+  // };
+
+  let content = (
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
+      // openTodo={(id) => {
+      //   setTodoId(id)
+      // }}
+      openTodo={setTodoId}
+    />
+  );
+
+  if (todoId) {
+    const selectedTodo = todos.find((todo) => todo.id === todoId);
+    content = (
+      <TodoScreen
+        onRemove={removeTodo}
+        goBack={() => setTodoId(null)}
+        todo={selectedTodo}
+        onSave={updateTodo}
+      />
+    );
+  }
 
   return (
     <View>
-        <Navbar title="Todo App"/>
-        <View style={styles.container}>
-            { content }
-        </View>
-        <StatusBar style="auto" />
+      <Navbar title="Todo App" />
+      <View style={styles.container}>{content}</View>
+      <StatusBar style="auto" />
     </View>
-  )}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      paddingHorizontal: THEME.PADDING_HORIZONTAL,
-      paddingVertical: 20
-    }
-  });
-  
+  container: {
+    paddingHorizontal: THEME.PADDING_HORIZONTAL,
+    paddingVertical: 20,
+  },
+});
