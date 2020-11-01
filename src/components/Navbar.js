880e7e01e41e47c40680af251b6a02768f79449d
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-01 07:50:21 +0200

    platform definition for a button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, Platform} from 'react-native'
 import { THEME } from '../theme'
 import { AppTextBold } from './ui/AppTextBold'
 
