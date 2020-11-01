4755fe62740e46e12650fcee09efdfdbda00dcf8
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-01 08:11:47 +0200

    adaptive styles ended

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, View, Button } from 'react-native'
+import { StyleSheet, View, Dimensions } from 'react-native'
 import { FontAwesome, AntDesign } from '@expo/vector-icons'
 import { THEME } from '../theme'
 import { AppCard } from '../components/ui/AppCard'
@@ -66,7 +66,8 @@ const styles = StyleSheet.create({
         padding: 15
     },
     button: {
-        width: '40%'
+        // width: Dimensions.get('window').width / 3
+        width: Dimensions.get('window') > 400 ? 150 : 100
     },
     title: {
         fontSize: 20
