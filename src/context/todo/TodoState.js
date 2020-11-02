a62a6e3e17d9ffd6628017f11b63f83f4569f58e
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-02 22:14:37 +0200

    deleting elements ended

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -48,8 +48,15 @@ export const TodoState = ({ children }) => {
         },
         {
           text: "Delete",
-          onPress: () => {
+          onPress: async () => {
             changeScreen(null);
+            await fetch(
+              `https://rn-todo-app-30322.firebaseio.com/todos/${id}.json`,
+              {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+              }
+            );
             dispatch({ type: REMOVE_TODO, id });
           },
         },
