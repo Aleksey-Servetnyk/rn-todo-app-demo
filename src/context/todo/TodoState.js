990c97435f25246cd03c9fd646201c9c54f98a20
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-02 11:37:03 +0200

    types & TodoState updated without FETCH function

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,13 +1,23 @@
 import React, { useContext, useReducer } from "react";
 import { Alert } from "react-native";
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types";
+import {
+  ADD_TODO,
+  CLEAR_ERROR,
+  HIDE_LOADER,
+  REMOVE_TODO,
+  SHOW_ERROR,
+  SHOW_LOADER,
+  UPDATE_TODO,
+} from "../types";
 import { TodoContext } from "./todoContext";
 import { todoReducer } from "./todoReducer";
 import { ScreenContext } from "../screen/screenContext";
 
 export const TodoState = ({ children }) => {
   const initialState = {
-    todos: [{ id: "1", title: "Learn React Native" }],
+    todos: [],
+    loading: false,
+    error: null,
   };
   const { changeScreen } = useContext(ScreenContext);
   const [state, dispatch] = useReducer(todoReducer, initialState);
@@ -35,6 +45,10 @@ export const TodoState = ({ children }) => {
     );
   };
   const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title });
+  const showLoader = () => dispatch({ type: SHOW_LOADER });
+  const hideLoader = () => dispatch({ type: HIDE_LOADER });
+  const showError = (error) => dispatch({ type: SHOW_ERROR, error });
+  const clearError = () => dispatch({ type: CLEAR_ERROR });
 
   return (
     <TodoContext.Provider
