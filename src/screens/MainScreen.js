a5f67a8211287563d758c9b42e70f7a885572425
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2020-11-02 10:05:33 +0200

    refactoring of the application ended

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,64 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, FlatList, Image, Dimensions } from 'react-native'
-import { AddTodo } from '../components/AddTodo'
-import { Todo } from '../components/Todo'
-import { THEME } from '../theme'
+import React, { useState, useEffect, useContext } from "react";
+import { StyleSheet, View, FlatList, Image, Dimensions } from "react-native";
+import { AddTodo } from "../components/AddTodo";
+import { Todo } from "../components/Todo";
+import { ScreenContext } from "../context/screen/screenContext";
+import { TodoContext } from "../context/todo/todoContext";
+import { THEME } from "../theme";
 
-export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
-    const [deviceWidth, setDeviceWidth] = useState(
-            Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
-        )
+export const MainScreen = () => {
+  const { addTodo, todos, removeTodo } = useContext(TodoContext);
+  const { changeScreen } = useContext(ScreenContext);
+  const [deviceWidth, setDeviceWidth] = useState(
+    Dimensions.get("window").width - THEME.PADDING_HORIZONTAL * 2
+  );
 
-    useEffect(() => {
-        const update = () => {
-            const width = Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
-            setDeviceWidth(width)
-        }
-        Dimensions.addEventListener('change', update)
-        return () => {
-            Dimensions.removeEventListener('change', update)
-        }
-    })
-    //const width = Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
-    let content
+  useEffect(() => {
+    const update = () => {
+      const width =
+        Dimensions.get("window").width - THEME.PADDING_HORIZONTAL * 2;
+      setDeviceWidth(width);
+    };
+    Dimensions.addEventListener("change", update);
+    return () => {
+      Dimensions.removeEventListener("change", update);
+    };
+  });
+  //const width = Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
+  let content;
 
-    if (todos.lenght == 0) {
-        content = (
-            <View style={styles.imgWrap}>
-                <Image 
-                    style={styles.image}
-                    source={require('../../assets/no-items.png')} 
-                />
-            </View>
-        )} else {
-            content =(
-                <View style={{ deviceWidth }}>
-                    <FlatList
-                        keyExtractor={item => item.id.toString()}
-                        data={todos}
-                        renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} /> }
-                    />
-                </View>
-        )}
+  if (todos.lenght == 0) {
+    content = (
+      <View style={styles.imgWrap}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/no-items.png")}
+        />
+      </View>
+    );
+  } else {
+    content = (
+      <View style={{ deviceWidth }}>
+        <FlatList
+          keyExtractor={(item) => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => (
+            <Todo todo={item} onRemove={removeTodo} onOpen={changeScreen} />
+          )}
+        />
+      </View>
+    );
+  }
 
-    return (
-        <View>
-            <AddTodo onSubmit={addTodo} />
-            {content}
-        </View>
-    )
-}
+  return (
+    <View>
+      <AddTodo onSubmit={addTodo} />
+      {content}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    imgWrap: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 10,
-        height: 300
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'contain'
-    }
-})
+  imgWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    height: 300,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
